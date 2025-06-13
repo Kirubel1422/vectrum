ef6804c60aa6fbc2c723fb8d9a7f9686d81fddd0
Kirubel1422 <kirubel739@gmail.com>
2025-06-13 18:17:57 -0400

    feat: general information changing service

--- lib/service/user.service.ts
+++ lib/service/user.service.ts
@@ -2,12 +2,14 @@ import { db } from "@/db";
 import { User } from "../types/user.types";
 import { usersTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { GeneralSchemaType } from "@/app/(protected)/dashboard/my-account/_lib/schema";
 
 export const UserService = {
   async saveUser(userData: Omit<User, "role">) {
     return await db
       .insert(usersTable)
       .values({
+        id: userData.id,
         email: userData.email,
         first_name: userData.first_name,
         last_name: userData.last_name,
@@ -21,5 +23,21 @@ export const UserService = {
       .select()
       .from(usersTable)
       .where(eq(usersTable.email, email));
+  },
+
+  async handleChangeGeneralInfo(
+    data: GeneralSchemaType
+  ): Promise<{ success: boolean; message: string }> {
+    const { first_name, last_name } = data;
+
+    if (!first_name || !last_name)
+      return { success: false, message: "All fields are required" };
+
+    await db.update(usersTable).set({
+      first_name,
+      last_name,
+    });
+
+    return { success: true, message: "Successfully updated info." };
   },
 };
