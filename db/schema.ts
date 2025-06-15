92e91aee10cbf05e07cb1b57617cd8733fb159d4
Kirubel1422 <kirubel739@gmail.com>
2025-06-15 13:39:01 -0400

    ref: removes pgEnum to fix a bug and redefines authUsers table

--- db/schema.ts
+++ db/schema.ts
@@ -1,27 +1,39 @@
-import { pgTable, varchar, uuid, timestamp, pgEnum } from "drizzle-orm/pg-core";
-import crypto from "node:crypto";
+import { sql } from "drizzle-orm";
+import {
+  pgTable,
+  varchar,
+  uuid,
+  timestamp,
+  pgSchema,
+} from "drizzle-orm/pg-core";
 
-export const ROLES = pgEnum("roles", ["user", "admin"]);
+export const ROLES = ["user", "admin"] as const;
 
-export const authUsers = pgTable("auth.users", {
-  id: uuid().primaryKey(),
+const authSchema = pgSchema("auth");
+
+export const authUsers = authSchema.table("users", {
+  id: uuid("id").primaryKey(),
 });
 
 export const usersTable = pgTable("users", {
   id: uuid()
     .primaryKey()
-    .references(() => authUsers.id, { onDelete: "cascade" }),
+    .references(() => authUsers.id, {
+      onDelete: "cascade",
+    }),
   first_name: varchar({ length: 255 }).notNull(),
   last_name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   created_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
   updated_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
   password: varchar({ length: 255 }).notNull(),
-  role: ROLES("role").default("user").notNull(),
+  role: varchar("role", { enum: ROLES }).default("user").notNull(),
 });
 
 export const resumesTable = pgTable("resumes", {
-  id: uuid().default(crypto.randomUUID()).primaryKey(),
+  id: uuid("id")
+    .default(sql`gen_random_uuid()`)
+    .primaryKey(),
   user_id: uuid()
     .notNull()
     .references(() => usersTable.id, { onDelete: "cascade" }),
